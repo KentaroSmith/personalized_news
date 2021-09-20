b32194a6cd7a09d5e26218247a55a989a1b00c3c
Dan <manthedan87@protonmail.com>
2021-09-19 21:00:16 -0500

    added new list of tech articles

--- showcase/views.py
+++ showcase/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
-import requests
+import requests, environ
+from datetime import datetime
+
+env = environ.Env()
+environ.Env.read_env()
 
 def index(req):
-    r = requests.get("http://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=440&count=3\r\n")
+    #steam news
+    r = requests.get("http://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=440&count=5\r\n")
     
     data = r.json()
     moar_data =data['appnews']
     even_moar_data = moar_data['newsitems']
+    articles = []
+    for article in even_moar_data:
+        print(article)
+        articles.append(article)
+    today = datetime.now()
+    print(today)
+    #Tech news
+    url = ('https://newsapi.org/v2/top-headlines?'
+        'country=us&'
+        'category=technology&'
+        'from='+str(today)+'&'
+        'sortBy=popularity&'
+        'apiKey='+str(env('NEWS_API_KEY')))
+
+    tech_array = []
+    response = requests.get(url)
+    reeeesponse = response.json()
+    filtered_response = reeeesponse['articles']
+    for article in filtered_response:
+        tech_array.append(article)
     return render(req, 'index.html', {
-        'steam_news': even_moar_data
+        'steam_news': articles,
+        'tech_news': tech_array
     })
